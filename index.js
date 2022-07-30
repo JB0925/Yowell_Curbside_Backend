a2e9b9ed93cd1f59645ff628870e190a95f9b94f
Jesse Brink <jbrink0925@gmail.com>
2022-07-30 13:53:08 -0400

    add middleware

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  if (req.hostname !== "nameless-wave-46063.herokuapp.com") {
+  if (req.headers.referer !== "https://nameless-wave-46063.herokuapp.com/") {
     console.log(req.socket.address().port);
     console.log(req.headers.referer);
     return res.status(403).json({
