64a97adb9d9c59897c549dc6dcb042b59fefecd5
Jesse Brink <jbrink0925@gmail.com>
2022-07-30 13:51:24 -0400

    add middleware

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(
 app.use((req, res, next) => {
   if (req.hostname !== "nameless-wave-46063.herokuapp.com") {
     console.log(req.socket.address().port);
-    console.log(req.get("host"));
+    console.log(req.headers.referer);
     return res.status(403).json({
       message: "This content may not be accessed via this method.",
       status: 403,
