df5f2bb15e175630242c9cc365e4e9ab79d722d9
Jesse Brink <jbrink0925@gmail.com>
2022-10-01 23:35:40 -0400

    change from single TCP connection client to connection pool for DB connection.

--- db.js
+++ db.js
@@ -1,21 +1,40 @@
 "use strict";
-/** Database setup for the Guitar App. */
+/** Database setup for the Yowell Curbside App. */
 require("dotenv").config();
 
-const { Client } = require("pg");
+// const { Client } = require("pg");
+const { Pool } = require("pg");
 const { getDatabaseUri } = process.env.DATABASE_URL || require("./config");
 let db;
 
+// if (process.env.NODE_ENV === "production") {
+//   db = new Client({
+//     connectionString: process.env.DATABASE_URL || getDatabaseUri(),
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   });
+// } else {
+//   db = new Client({
+//     connectionString: getDatabaseUri(),
+//   });
+// }
 if (process.env.NODE_ENV === "production") {
-  db = new Client({
+  db = new Pool({
     connectionString: process.env.DATABASE_URL || getDatabaseUri(),
+    max: 20,
+    connectionTimeoutMillis: 0,
+    idleTimeoutMillis: 0,
     ssl: {
       rejectUnauthorized: false,
     },
   });
 } else {
-  db = new Client({
+  db = new Pool({
     connectionString: getDatabaseUri(),
+    max: 20,
+    connectionTimeoutMillis: 0,
+    idleTimeoutMillis: 0,
   });
 }
 
