51160050d5eedc49836de9db52e3b1e4b1790771
Jesse Brink <jbrink0925@gmail.com>
2022-10-08 23:13:26 -0400

    continue to try to setup CI / CD via GitHub Actions.

---
+++ ci_db.js
@@ -0,0 +1,61 @@
+const { Pool } = require("pg");
+
+const DB_NAME = "curbside_test";
+
+const db = new Pool({
+  connectionString: DB_NAME,
+  max: 20,
+  connectionTimeoutMillis: 0,
+  idleTimeoutMillis: 0,
+});
+
+db.connect(async (err) => {
+  if (err) throw err;
+  await db.query(`CREATE DATABASE curbside_test`);
+
+  await db.query(
+    `CREATE TABLE IF NOT EXISTS students (
+     number PRIMARY KEY VARCHAR NOT NULL UNIQUE,
+     name VARCHAR(50) NOT NULL,
+     isloaded BOOLEAN DEFAULT false,
+     time VARCHAR,
+     added BOOLEAN DEFAULT false
+    )`
+  );
+
+  await db.query(
+    `CREATE TABLE IF NOT EXISTS temp_students (
+     name VARCHAR(50) NOT NULL,
+     isloaded BOOLEAN DEFAULT false,
+     time VARCHAR,
+     added BOOLEAN DEFAULT false    
+    )`
+  );
+});
+
+// async function setupDatabase() {
+//   await db.query("CREATE DATABASE IF NOT EXISTS curbside_test");
+
+//   await db.query(
+//     `CREATE TABLE IF NOT EXISTS students (
+//      number PRIMARY KEY VARCHAR NOT NULL UNIQUE,
+//      name VARCHAR(50) NOT NULL,
+//      isloaded BOOLEAN DEFAULT false,
+//      time VARCHAR,
+//      added BOOLEAN DEFAULT false
+//     )`
+//   );
+
+//   await db.query(
+//     `CREATE TABLE IF NOT EXISTS temp_students (
+//      name VARCHAR(50) NOT NULL,
+//      isloaded BOOLEAN DEFAULT false,
+//      time VARCHAR,
+//      added BOOLEAN DEFAULT false
+//     )`
+//   );
+// }
+
+// setupDatabase();
+
+module.exports = db;
--- ci_db_setup.sql
+++ ci_db_setup.sql
@@ -4,15 +4,15 @@ CREATE DATABASE curbside_test;
 
 \c curbside_test
 
-CREATE TABLE students (
-  number PRIMARY KEY VARCHAR NOT NULL UNIQUE,
+CREATE TABLE IF NOT EXISTS students (
+  number VARCHAR PRIMARY KEY NOT NULL UNIQUE,
   name VARCHAR(50) NOT NULL,
   isloaded BOOLEAN DEFAULT false,
   time VARCHAR,
   added BOOLEAN DEFAULT false    
 )
 
-CREATE TABLE temp_students (
+CREATE TABLE IF NOT EXISTS temp_students (
   name VARCHAR(50) NOT NULL,
   isloaded BOOLEAN DEFAULT false,
   time VARCHAR,
