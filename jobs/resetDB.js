c8b0617f70819800d1f92aefa3a2333f3329ce34
Jesse Brink <jbrink0925@gmail.com>
2022-11-30 00:44:25 -0500

    add twilio for SMS alerts in the event that the Heroku scheduler job fails. Bump app version.

--- jobs/resetDB.js
+++ jobs/resetDB.js
@@ -1,4 +1,9 @@
 const db = require("../db");
+require("dotenv").config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
 
 const reset = async () => {
   await db.query(
@@ -12,7 +17,26 @@ const reset = async () => {
   await db.query(`DELETE FROM temp_students`);
 };
 
-reset().then(() => {
-  console.log("All done!");
-  process.exit();
-});
+reset()
+  .then(async () => {
+    const query = await db.query(`SELECT isloaded FROM students`);
+    return query.rows.filter((q) => q.isloaded);
+  })
+  .then((arr) => {
+    if (arr.length) {
+      const id = client.messages.create({
+        body: "Heroku Curbside App DB did not reset properly; see Heroku for details.",
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: process.env.RECEIVER_PHONE_NUMBER,
+      });
+      return id;
+    }
+  })
+  .then((message) => {
+    if (message) {
+      console.log(message.sid);
+    } else {
+      console.log("All done!");
+    }
+    process.exit();
+  });
