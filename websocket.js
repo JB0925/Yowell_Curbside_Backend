d2d432326ebca6fd1d8b380a6c1eef5118551f63
Jesse Brink <jbrink0925@gmail.com>
2023-10-17 09:03:46 -0400

    Revert "Make heartbeat interval longer."
    This reverts commit 3bde801b422cad1e0fd17ecd7a8e8dd5d3ecb208.

--- websocket.js
+++ websocket.js
@@ -38,7 +38,7 @@ function setupWebSocket(server) {
   // what to do after a connection is established
   wss.on("connection", (ctx) => {
     // handle message events
-    setInterval(ping, 30000);
+    setInterval(ping, 15000);
     ctx.on("message", async (message) => {
       try {
         // // handle connection keepalive messages
@@ -107,7 +107,11 @@ function setupWebSocket(server) {
       console.log(
         `websocket:: onClose - attempting to reconnect to ${wss.clients.size} clients`
       );
+      // ping();
     });
+
+    // sent a message that we're good to proceed
+    // ctx.send("connection established.");
   });
 }
 
