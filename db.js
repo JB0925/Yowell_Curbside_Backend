0ca2699f41dc2e8f75a829a92a81584fb4e86ba6
Jesse Brink <jbrink0925@gmail.com>
2024-01-21 00:45:02 -0500

    testing GitHub Actions.

--- db.js
+++ db.js
@@ -2,23 +2,10 @@
 /** Database setup for the Yowell Curbside App. */
 require("dotenv").config();
 
-// const { Client } = require("pg");
 const { Pool } = require("pg");
 const { getDatabaseUri } = process.env.DATABASE_URL || require("./config");
 let db;
 
-// if (process.env.NODE_ENV === "production") {
-//   db = new Client({
-//     connectionString: process.env.DATABASE_URL || getDatabaseUri(),
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   });
-// } else {
-//   db = new Client({
-//     connectionString: getDatabaseUri(),
-//   });
-// }
 if (process.env.NODE_ENV === "production") {
   db = new Pool({
     connectionString: process.env.DATABASE_URL || getDatabaseUri(),
